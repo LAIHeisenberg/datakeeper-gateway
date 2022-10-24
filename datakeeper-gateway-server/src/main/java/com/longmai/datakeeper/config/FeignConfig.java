6c9abe1b5b117ef015cd077554ac5a960e6020da
laiyz <laiyaozong@longmai.com.cn>
2022-10-24 10:54:03 +0800

    移除无用的依赖

--- datakeeper-gateway-server/src/main/java/com/longmai/datakeeper/config/FeignConfig.java
+++ datakeeper-gateway-server/src/main/java/com/longmai/datakeeper/config/FeignConfig.java
@@ -1,21 +1,27 @@
 package com.longmai.datakeeper.config;
 
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import com.longmai.datakeeper.rest.api.ApiMaskingHandler;
+import feign.Feign;
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
 
-import java.util.stream.Collectors;
 
 
 @Configuration
 public class FeignConfig {
+
+    @Value("${feign.client.url}")
+    private String client_url;
+
     @Bean
-    @ConditionalOnMissingBean
-    public HttpMessageConverters messageConverters(ObjectProvider<HttpMessageConverter<?>> converters) {
-        return new HttpMessageConverters(converters.orderedStream().collect(Collectors.toList()));
+    public ApiMaskingHandler getApiMaskingHandler(){
+        ApiMaskingHandler target = Feign.builder().encoder(new GsonEncoder()).decoder(new GsonDecoder())
+                .target(ApiMaskingHandler.class, client_url);
+        return target;
     }
+
 }
 
